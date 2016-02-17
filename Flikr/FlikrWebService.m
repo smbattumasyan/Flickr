ed00f39af4d3221c91d6a9e727e80848f06aabee
Smbat Tumasyan <smbatt@davitg-mac.local>
2016-02-17 19:05:13 +0400

    fixed requests

--- Flikr/FlikrWebService.m
+++ Flikr/FlikrWebService.m
@@ -18,7 +18,7 @@
 - (void)imagesRequest:(void (^)(NSData * _Nullable, NSURLResponse * _Nullable, NSError * _Nullable))completionHandler
 {
     NSURLSession *session          = [NSURLSession sharedSession];
-    NSString *urlString            = @"https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=bcca5e8cd331fd3376b3f82a1d9c74ab&user_id=138446653%40N07&format=json&nojsoncallback=1&auth_token=72157662443260953-935293da69c44fb8&api_sig=7192cfbcded9738998a94c26139d47a8";
+    NSString *urlString            = @"https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=76d55f7653f69d24d85728b5e760d552&user_id=138446653%40N07&format=json&nojsoncallback=1";
     NSURLSessionDataTask *dataTask = [session dataTaskWithURL:[NSURL URLWithString:urlString] completionHandler:completionHandler];
     [dataTask resume];
 }
@@ -26,7 +26,7 @@
 - (void)imageRequest:(NSString*)photoID completionHandler:(void (^)(NSData * _Nullable, NSURLResponse * _Nullable, NSError * _Nullable))completionHandler
 {
     NSURLSession *session          = [NSURLSession sharedSession];
-    NSString *urlString = [NSString stringWithFormat:@"https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=76d55f7653f69d24d85728b5e760d552&photo_id=%@&secret=795c4cef2fdc0001&format=json&nojsoncallback=1&auth_token=72157664714269566-dc01c33f5dac4862&api_sig=9309507d4be14b9a9268baca52df6a85",photoID];
+    NSString *urlString = [NSString stringWithFormat:@"https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=76d55f7653f69d24d85728b5e760d552&photo_id=%@&format=json&nojsoncallback=1",photoID];
     NSURLSessionDataTask *dataTask = [session dataTaskWithURL:[NSURL URLWithString:urlString] completionHandler:completionHandler];
     [dataTask resume];
 }
